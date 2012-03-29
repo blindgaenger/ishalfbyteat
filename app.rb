a5da30cd2e201d09d2464cdcf89df977b8a93d69
blindgaenger <blindgaenger@gmail.com>
2012-03-29 12:25:15 +0200

    your pages, sir

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ configure do
 end
 
 helpers do
+  # checking the user where he is right now
+  # may result in multiple venues at the same time
   def here_now?(user_id, venue_id)
     venue = $foursquare.venue(venue_id)
     return nil unless venue
@@ -30,8 +32,21 @@ helpers do
 end
 
 get '/' do
-  @here = here_now?(ENV['FOURSQUARE_USER_ID'], ENV['FOURSQUARE_VENUE_ID'])
-  haml :index
+  if venue = params[:venue]
+    redirect to("/#{URI.encode venue}")
+  else
+    haml :home
+  end
+end
+
+get '/:venue' do
+  @venue = params[:venue]
+  if @venue == ENV['FOURSQUARE_VENUE_NAME']
+    @here = here_now?(ENV['FOURSQUARE_USER_ID'], ENV['FOURSQUARE_VENUE_ID'])
+    haml :venue_known
+  else
+    haml :venue_unknown
+  end
 end
 
 get '/test/:user_id/:venue_id' do
